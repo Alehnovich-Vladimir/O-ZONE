8ca19f09381e21f24d1e9622256faf7b7aa26fd8
AlexVovan <alehnovich.vova@gmail.com>
2021-12-23 22:22:21 +0100

    add db-json and render-data

---
+++ src/modules/getData.js
@@ -0,0 +1,8 @@
+const getData = () => {
+    return fetch('https://test-203a1-default-rtdb.firebaseio.com/goods.json')
+        .then(response => {
+        return response.json()
+        })
+}
+
+export default getData
