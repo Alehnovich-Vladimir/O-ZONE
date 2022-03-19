7ccd55db0ec1bb8a23a7689ff022e4458cefb923
AlexVovan <alehnovich.vova@gmail.com>
2022-03-19 15:20:33 +0100

    add new database

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,5 +1,5 @@
 const getData = () => {
-    return fetch(`https://ozonchik-1048f-default-rtdb.firebaseio.com/goods.json`)
+    return fetch(`https://ozon-f002a-default-rtdb.firebaseio.com/goods.json`)
         .then(response => {
         return response.json()
         })
